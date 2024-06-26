d7012b3040181a119da3822efce625ac7eebe7f4
圣迹 <76844630+snugbrick@users.noreply.github.com>
2024-06-26 21:23:29 +0800

    add check

--- src/main/java/org/m1arcleur/sakuratech/SakuraTech.java
+++ src/main/java/org/m1arcleur/sakuratech/SakuraTech.java
@@ -1,13 +1,14 @@
 package org.m1arcleur.sakuratech;
 
-import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import org.bukkit.plugin.java.JavaPlugin;
 import org.m1arcleur.sakuratech.item.mainRegister;
 import org.m1arcleur.sakuratech.listener.bowUnitListener;
 import org.m1arcleur.sakuratech.listener.notPlaceable;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 
 /**
  * 该插件的终极目标是实现：综合性樱花立场生成器
@@ -15,11 +16,12 @@ import javax.annotation.Nullable;
  * 2.樱花原锭
  * 3.樱花核心
  */
-/*要接着做的:使樱花工作台返回的是SLimefunItemStack,
-
+/*
+ * 要接着做的:使樱花工作台返回的是SLimefunItemStack,
+ * 
  * 处理构思：将第二个配方中的物品切换（我估计没用
-
- * */
+ * 
+ */
 public final class SakuraTech extends JavaPlugin implements SlimefunAddon {
     private static SakuraTech Instance;
 
@@ -30,9 +32,16 @@ public final class SakuraTech extends JavaPlugin implements SlimefunAddon {
         if (getServer().getPluginManager().getPlugin("sakuraTech") != null) {
             getLogger().info("### sakuraTch v1.0.0 ###");
             getLogger().info("=-----by:MiralceUR-----=");
+        } else if (getServer().getPluginManager().getPlugin("Slimefun") == null) {
+            getLogger().info("无法检测到Slimefun插件前置");
+            if (getServer().getPluginManager().getPlugin("Slimefun").isEnabled()) {
+                getLogger().info("Slimefun插件前置未加载完毕");
+                getServer().getPluginManager().disablePlugin(this);
+            }
+            getServer().getPluginManager().disablePlugin(this);
         } else {
             getLogger().info("无法检测到sakuraTech插件");
-            return;
+            getServer().getPluginManager().disablePlugin(this);
         }
 
         mainRegister.multiMahcineRegister();
@@ -44,15 +53,18 @@ public final class SakuraTech extends JavaPlugin implements SlimefunAddon {
         getLogger().info("插件已启用");
 
         /*
-        ItemStack IS = new CustomItemStack(SkullItem.fromURL("(http://textures.minecraft.net/texture/e424a686b1c248e71a39201322a70d42cf5edd1e3afe7cdc5f6fee085fb2bcc0"),
-                "Name","Lore");
- */
+         * ItemStack IS = new CustomItemStack(SkullItem.fromURL(
+         * "(http://textures.minecraft.net/texture/e424a686b1c248e71a39201322a70d42cf5edd1e3afe7cdc5f6fee085fb2bcc0"
+         * ),
+         * "Name","Lore");
+         */
         /*
          * ItemStack IS = new CustomItem(SkullItem.fromBase64(),"Name","Lore");
          *
          * Player player = getServer().getPlayer("a");
-         * player.getWorld().spawnParticle(Particle.ASH, player.getLocation(), 0, 0, 0, 0,int speed);
-         * */
+         * player.getWorld().spawnParticle(Particle.ASH, player.getLocation(), 0, 0, 0,
+         * 0,int speed);
+         */
     }
 
     public void listenerRgister() {
